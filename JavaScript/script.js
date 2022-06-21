dbf8a0a766ca070a4d480338ef426192e0ed8304
Sockstealer21 <michaelsuushi@gmail.com>
2022-06-21 16:02:43 -0700

    Gave Drawing divs a class name of drawingSpace

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,6 @@
 for (let x = 0; x<256;x++){
     const drawingDivs = document.createElement('div')
+    drawingDivs.setAttribute('class', 'drawingSpace');
     container.appendChild(drawingDivs)
 }
 
