3a4c846c79464dd3d17179900e526d1db8651836
Sockstealer21 <michaelsuushi@gmail.com>
2022-06-22 19:00:06 -0700

    Create button with click event function newPad which wipes the page clean and sets new grid amount

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,27 +1,61 @@
 
-
 for (let x = 0; x<256;x++){
     const drawingDivs = document.createElement('div')
     drawingDivs.setAttribute('class', 'drawingSpace');
     container.appendChild(drawingDivs)
 }
 
-const drawing = container.childNodes
+const contChildren = container.childNodes
 
-drawing.forEach ((div) => {
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'red';
-});
-})
+drawing()
 
-/*drawing.forEach ((div) => {
-    div.addEventListener('mouseout', () => {
-        
-        div.style.backgroundColor = 'white';
-});
-})
-*/
-
-function colorChange() {
-    
+function drawing() {
+    contChildren.forEach ((div) => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'red';
+        });
+    })
 }
+
+function clearing(){
+    contChildren.forEach ((div) => {
+        div.style.backgroundColor = 'white';
+    })
+}
+
+const refreshButton = document.createElement('button')
+refreshButton.textContent = 'New page'
+const body = document.querySelector('body')
+body.insertBefore(refreshButton, container)
+
+refreshButton.addEventListener('click', () => {
+    newPad()
+});
+
+function childRemover(){
+    let e = document.querySelector('div')
+    var first = e.firstElementChild;
+    while (first) {
+        first.remove();
+        first = e.firstElementChild;
+    }
+}
+
+function newPad (){
+    let gridNumber = prompt('How many square per side? 100 is the maximum.');
+    if (gridNumber > 100) {
+        alert('No Number higher than 100 please.')
+    } else {
+        document.getElementById('container').style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        document.getElementById('container').style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+        childRemover()
+        for (let x = 0; x < (gridNumber * gridNumber); x++ ){
+            const drawingDivs = document.createElement('div')
+            drawingDivs.setAttribute('class', 'drawingSpace');
+            container.appendChild(drawingDivs)
+            
+            }
+        drawing()
+        }
+        clearing()
+    }
