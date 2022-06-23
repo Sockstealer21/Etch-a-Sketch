0614ffe13dfc8a29b81fc09e6b4cac1c30b7bf2d
Sockstealer21 <michaelsuushi@gmail.com>
2022-06-22 17:25:17 -0700

    Create mouseover EventListener that changes container child div background color to red

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,27 @@
+
+
 for (let x = 0; x<256;x++){
     const drawingDivs = document.createElement('div')
     drawingDivs.setAttribute('class', 'drawingSpace');
     container.appendChild(drawingDivs)
 }
 
+const drawing = container.childNodes
+
+drawing.forEach ((div) => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'red';
+});
+})
+
+/*drawing.forEach ((div) => {
+    div.addEventListener('mouseout', () => {
+        
+        div.style.backgroundColor = 'white';
+});
+})
+*/
+
+function colorChange() {
+    
+}
